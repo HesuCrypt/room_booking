00592fd8ca731ebc826c5fa2cb8a0c34b5f5337d
HesuCrypt <louissebertillo2004@gmail.com>
2026-05-04 11:40:31 +0800

    style: use inset-0 for all bookings and add more crowns to gold theme

--- src/App.tsx
+++ src/App.tsx
@@ -649,12 +649,13 @@ function RoomBookingPage() {
                       {booking ? (
                         <motion.div
                           layoutId={`booking-${booking.id}`}
-                          className={`absolute inset-1 p-1.5 flex flex-col justify-between group overflow-hidden ${getBookingClass(booking)}`}
+                          className={`absolute inset-0 p-1.5 flex flex-col justify-between group overflow-hidden ${getBookingClass(booking)}`}
                         >
                           {booking.theme === 'gold' && (
                             <>
-                              <Crown className="absolute -top-0.5 -right-0.5 w-3.5 h-3.5 text-black/80 z-20 rotate-12" />
-                              <Crown className="absolute top-0.5 left-0.5 w-2.5 h-2.5 text-black/30 z-20 -rotate-12" />
+                              <Crown className="absolute -top-1 -right-1 w-4.5 h-4.5 text-black/90 z-20 rotate-12 drop-shadow-sm" />
+                              <Crown className="absolute top-0.5 left-0.5 w-3 h-3 text-black/30 z-20 -rotate-12" />
+                              <Crown className="absolute -bottom-1 left-1/2 -translate-x-1/2 w-2.5 h-2.5 text-black/20 z-20" />
                               <Sparkles className="absolute bottom-1 right-1 w-2.5 h-2.5 text-black/40 z-20 animate-pulse" />
                             </>
                           )}
